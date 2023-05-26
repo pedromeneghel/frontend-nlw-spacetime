95f476d2cf353316bb6c795abea5db20842a4653
Pedro Meneghel <pedromeneghel@gmail.com>
2023-05-26 15:40:58 -0300

    refactor: adiciona estilização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="text-2xl font-bold uppercase">aoi</div>
+  return (
+    <div className="h-screen bg-zinc-900 p-6 text-2xl font-bold uppercase text-zinc-50">
+      <h1>Olá mundo</h1>
+    </div>
+  )
 }
